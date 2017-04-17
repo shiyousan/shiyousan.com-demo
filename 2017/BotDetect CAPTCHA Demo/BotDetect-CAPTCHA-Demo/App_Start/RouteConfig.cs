2e3791d26710a99f9479355f698125bc68b8f2a3
ywfroad <ywfroad@hotmail.com>
2017-04-17 17:56:00 +0800

    验证码插件演示完善

---
+++ 2017/BotDetect CAPTCHA Demo/BotDetect-CAPTCHA-Demo/App_Start/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BotDetect_CAPTCHA_Demo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*botdetect}", new { botdetect = @"(.*)BotDetectCaptcha\.ashx" });
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ 2017/BotDetect CAPTCHA Demo/BotDetect-CAPTCHA-Demo/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BotDetect.Web.Mvc;
+
+namespace BotDetect_CAPTCHA_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [CaptchaValidation("CaptchaCode", "ExampleCaptcha", "验证码输入错误！！！")]
+        public ActionResult Index(string msg, string CaptchaCode)
+        {
+            //ExampleCaptcha.SoundEnabled = false; 
+            
+            /*
+             (Option) CAPTCHA Validation Separate From Model State Validation
+
+If, for any reason, you want to check Captcha validity separately from ModelState, you can simply use an additional action parameter (which must be called captchaValid) and make the validation logic explicit
+public ActionResult ExampleAction(ExampleModel model, bool captchaValid)
+{
+    if (captchaValid)
+    {
+    
+        […]
+             */
+            //if (!ModelState.IsValid)
+            //{
+            //    // TODO: Captcha validation failed, show error message      
+            //    ViewBag.Msg = CaptchaCode;
+            //}
+            //else
+            //{
+            //    // TODO: Captcha validation passed, proceed with protected action  
+            //    ViewBag.Msg = msg;
+            //}
+
+            return View();
+        }
+    }
+}
