57249a378d8713d10d6f13fa0937c321e3e37999
ywfroad <ywfroad@hotmail.com>
2017-06-26 16:15:07 +0800

    添加使用正则删除UTF8特殊字符演示代码

---
+++ 2017/DeleteUtf8SpecialSymbolsByRegular/DeleteUtf8SpecialSymbolsByRegular/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DeleteUtf8SpecialSymbolsByRegular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = "测•试�字符";
+            Console.WriteLine(content);
+
+            var regex = new Regex(@"[^\u4E00-\u9FA5\u3000-\u303F\uFF00-\uFFEF\u0000-\u007F\u201c-\u201d]", RegexOptions.Multiline | RegexOptions.ExplicitCapture);
+            content = regex.Replace(content, "");
+            //content=content.Replace("�", "").Replace("•", "");
+
+            Console.WriteLine(content);
+            Console.ReadKey();
+        }
+    }
+}
