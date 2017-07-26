cb385caba259ddb63f164b04b959ee53694fae99
ywfroad <ywfroad@hotmail.com>
2017-07-26 17:58:44 +0800

    添加令牌演示项目

---
+++ 2017/AntiForgeryToken-Demo/AntiForgeryToken-Demo/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AntiForgeryToken_Demo.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Login(string ReturnUrl)
+        {
+            ViewBag.ReturnUrl = ReturnUrl;
+            if (User.Identity.IsAuthenticated)
+            {
+                //防止欺诈跳转（回调地址为空也会等于false）
+                if (Url.IsLocalUrl(ReturnUrl))
+                {
+                    return Redirect(ReturnUrl);
+                }
+                else
+                {
+                    return Redirect("/Home/Index");
+                }
+            }
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string ReturnUrl, string UserName ="test")
+        {        
+            FormsAuthentication.SetAuthCookie(UserName , true);
+
+            //防止欺诈跳转
+            if (!Url.IsLocalUrl(ReturnUrl))
+            {
+                return Redirect("/Home/Index");
+            }
+            else
+            {
+                return Redirect(ReturnUrl);
+            }            
+        }
+
+        /// <summary>
+        /// 账号注销
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("/Home/Index");
+        }
+
+    }
+}
---
+++ 2017/AntiForgeryToken-Demo/AntiForgeryToken-Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AntiForgeryToken_Demo.MvcApplication" Language="C#" %>
